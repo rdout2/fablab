1a94d85d446ab65dd3bc01511173d3a39d538318
unknown <rahmatmahamatdout@gmail.com>
2023-04-11 11:16:33 +0100

    Premier essai

---
+++ FABLAB/admin.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from django.contrib import admin
+
+
+from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import *
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['email', 'first_name', 'last_name', 'active', 'timestamp']
+    list_display_links  = ['email',]
+    list_filter         = ['email']
+    search_fields       = ['email', 'first_name', 'last_name']
+    list_per_page       = 25
+    class Meta:
+        model = Contact
+admin.site.register(Contact, ContactAdmin)
+
+class BlogCategoryAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['name', 'active', 'timestamp', 'updated']
+    list_display_links  = ['name',]
+    list_filter         = ['name']
+    search_fields       = ['name', ]
+    list_per_page       = 25
+    class Meta:
+        model = BlogCategory
+admin.site.register(BlogCategory, BlogCategoryAdmin)
+
+class TrainingCategoryAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['name', 'active', 'timestamp', 'updated']
+    list_display_links  = ['name',]
+    list_filter         = ['name']
+    search_fields       = ['name', ]
+    list_per_page       = 25
+    class Meta:
+        model = TrainingCategory
+admin.site.register(TrainingCategory, TrainingCategoryAdmin)
+
+class MachineCategoryAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['name', 'active', 'timestamp', 'updated']
+    list_display_links  = ['name',]
+    list_filter         = ['name']
+    search_fields       = ['name',]
+    list_per_page       = 25
+    class Meta:
+        model = MachineCategory
+admin.site.register(MachineCategory, MachineCategoryAdmin)
+
+class GaleryAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['image', 'active', 'timestamp', 'updated']
+    list_display_links  = ['image',]
+    list_filter         = ['image']
+    search_fields       = ['image',]
+    list_per_page       = 25
+    class Meta:
+        model = Galery
+admin.site.register(Galery, GaleryAdmin)
+
+class BlogAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['name', 'active', 'timestamp', 'updated']
+    list_display_links  = ['name',]
+    list_filter         = ['name']
+    search_fields       = ['name',]
+    list_per_page       = 25
+    class Meta:
+        model = Blog
+admin.site.register(Blog, BlogAdmin)
+
+class TrainingAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['name', 'active', 'timestamp', 'updated']
+    list_display_links  = ['name',]
+    list_filter         = ['name']
+    search_fields       = ['name',]
+    list_per_page       = 25
+    class Meta:
+        model = Training
+admin.site.register(Training, TrainingAdmin)
+
+class MachineAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['name', 'active', 'timestamp', 'updated']
+    list_display_links  = ['name',]
+    list_filter         = ['name']
+    search_fields       = ['name',]
+    list_per_page       = 25
+    class Meta:
+        model = Machine
+admin.site.register(Machine, MachineAdmin)
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['email', 'active', 'timestamp', 'updated']
+    list_display_links  = ['email',]
+    list_filter         = ['email']
+    search_fields       = ['email',]
+    list_per_page       = 25
+    class Meta:
+        model = Newsletter
+admin.site.register(Newsletter, NewsletterAdmin)
+
+class TrustpilotAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['email','active', 'timestamp', 'updated']
+    list_display_links  = ['email',]
+    list_filter         = ['email']
+    search_fields       = ['email',]
+    list_per_page       = 25
+    class Meta:
+        model = Trustpilot
+admin.site.register(Trustpilot, TrustpilotAdmin)
+
+
+class AdmissionAdmin(admin.ModelAdmin):
+    date_hierarchy      = 'timestamp'
+    list_display        = ['email', 'first_name','Training', 'last_name', 'active', 'timestamp']
+    list_display_links  = ['email',]
+    list_filter         = ['email']
+    search_fields       = ['email', 'first_name', 'last_name']
+    list_per_page       = 25
+    class Meta:
+        model = Admission
+admin.site.register(Admission, AdmissionAdmin)
---
+++ FABLAB/models.py
@@ -0,0 +1,264 @@
+
+from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your models here.
+class TrainingCategory(models.Model):
+    name = models.CharField(_("Nom"),max_length=255, null=False, blank=False, unique=True)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+    slug        = models.SlugField(_("Slug"), max_length=255, null=True, blank=True, editable=False, unique=False)
+
+    def __str__(self):
+        return self.name
+
+def create_training_cat_slug(instance, new_slug=None):
+    slug = slugify(instance.name)
+    if new_slug is not None:
+        slug = new_slug
+    ourQuery = TrainingCategory.objects.filter(slug=slug)
+    exists = ourQuery.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, ourQuery.first().id)
+        return create_training_cat_slug(instance, new_slug=new_slug)
+    return slug
+
+def presave_training_cat_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_training_cat_slug(instance)
+pre_save.connect(presave_training_cat_slug, sender=TrainingCategory)
+
+
+
+
+
+
+
+class Training(models.Model) :
+    category = models.ForeignKey(TrainingCategory, on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(_("Nom"),max_length=255, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+    slug        = models.SlugField(_("Slug"), max_length=255, null=True, blank=True, editable=False, unique=False)
+    image = models.ImageField(upload_to = 'media', null=False, blank=False)
+   
+    def __str__(self):
+        return self.name
+
+def create_training_slug(instance, new_slug=None):
+    slug = slugify(instance.name)
+    if new_slug is not None:
+        slug = new_slug
+    ourQuery = Training.objects.filter(slug=slug)
+    exists = ourQuery.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, ourQuery.first().id)
+        return create_training_slug(instance, new_slug=new_slug)
+    return slug
+
+def presave_training_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_training_slug(instance)
+pre_save.connect(presave_training_slug, sender=Training)
+
+
+
+
+
+
+class BlogCategory (models.Model):
+    name = models.CharField(_("Nom"),max_length=255, null=False, blank=False, unique=True)
+    slug        = models.SlugField(_("Slug"), max_length=255, null=True, blank=True, editable=False, unique=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+   
+    def __str__(self):
+        return self.name
+
+def create_Blog_cat_slug(instance, new_slug=None):
+    slug = slugify(instance.name)
+    if new_slug is not None:
+        slug = new_slug
+    ourQuery = BlogCategory.objects.filter(slug=slug)
+    exists = ourQuery.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, ourQuery.first().id)
+        return create_Blog_cat_slug(instance, new_slug=new_slug)
+    return slug
+
+def presave_Blog_cat_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_Blog_cat_slug(instance)
+pre_save.connect(presave_Blog_cat_slug, sender=BlogCategory)
+
+
+
+
+
+
+
+class Blog(models.Model) :
+    category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(_("Nom"),max_length=255, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+    image = models.ImageField(upload_to = 'media', null=True, blank=True)
+    slug        = models.SlugField(_("Slug"), max_length=255, null=True, blank=True, editable=False, unique=False)
+
+    def __str__(self):
+        return self.name
+
+def create_Blog_slug(instance, new_slug=None):
+    slug = slugify(instance.name)
+    if new_slug is not None:
+        slug = new_slug
+    ourQuery = Blog.objects.filter(slug=slug)
+    exists = ourQuery.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, ourQuery.first().id)
+        return create_Blog_slug(instance, new_slug=new_slug)
+    return slug
+
+def presave_Blog_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_Blog_slug(instance)
+pre_save.connect(presave_Blog_slug, sender=Blog)   
+
+
+
+
+
+class MachineCategory(models.Model):
+    name = models.CharField(_("Nom"),max_length=255, null=False, blank=False, unique=True)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+    slug        = models.SlugField(_("Slug"), max_length=255, null=True, blank=True, editable=False, unique=False)
+
+    def __str__(self):
+        return self.name
+
+def create_machine_cat_slug(instance, new_slug=None):
+    slug = slugify(instance.name)
+    if new_slug is not None:
+        slug = new_slug
+    ourQuery = MachineCategory.objects.filter(slug=slug)
+    exists = ourQuery.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, ourQuery.first().id)
+        return create_machine_cat_slug(instance, new_slug=new_slug)
+    return slug
+
+def presave_machine_cat_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_machine_cat_slug(instance)
+pre_save.connect(presave_machine_cat_slug, sender=MachineCategory)
+
+
+
+
+
+
+
+class Machine(models.Model):
+    category = models.ForeignKey(MachineCategory, on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(_("Nom"),max_length=255, null=False, blank=False)
+    description = models.TextField(_("Description"),null=False, blank=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+    image = models.ImageField(upload_to = 'media', null=False, blank=False)
+    slug        = models.SlugField(_("Slug"), max_length=255, null=True, blank=True, editable=False, unique=False)
+    def __str__(self):
+        return self.name
+
+def create_machine_slug(instance, new_slug=None):
+    slug = slugify(instance.name)
+    if new_slug is not None:
+        slug = new_slug
+    ourQuery = Machine.objects.filter(slug=slug)
+    exists = ourQuery.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, ourQuery.first().id)
+        return create_machine_slug(instance, new_slug=new_slug)
+    return slug
+
+def presave_machine_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_machine_slug(instance)
+pre_save.connect(presave_machine_slug, sender=Machine)
+
+
+
+
+
+
+class Galery(models.Model) :
+    image = models.ImageField(upload_to = 'media', null=False, blank=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+    def __str__(self):
+        return str(self.id)
+
+
+class Newsletter(models.Model) :
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+    def __str__(self):
+        return (self.email)
+
+class Trustpilot(models.Model) :
+    first_name = models.CharField(_("Nom"),max_length=255, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    comment = models.TextField(null=False, blank=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+    def __str__(self):
+        return (self.email)
+
+
+class Admission(models.Model) :
+    first_name = models.CharField(_("Nom"),max_length=255, null=False, blank=False)
+    last_name = models.CharField(_("Prenom"),max_length=255, null=False, blank=False)
+    phone = models.CharField(_("Numero"),max_length=255, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    Training =  models.ForeignKey(Training, on_delete=models.CASCADE, null=False, blank=False)
+    cv = models.ImageField(upload_to = 'media', null=False, blank=False)
+    Motivation = models.TextField(null=False, blank=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+
+
+
+class Contact(models.Model) :
+    first_name = models.CharField(_("Nom"),max_length=255, null=False, blank=False)
+    last_name = models.CharField(_("Prenom"),max_length=255, null=False, blank=False)
+    phone = models.CharField(_("Numero"),max_length=255, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    subject = models.CharField(_("Sujet"),max_length=255, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+    active     = models.BooleanField(_("Active"), default=True)
+    timestamp  = models.DateTimeField(_("Created At"), auto_now_add=True, auto_now=False)
+    updated    = models.DateTimeField(_("Updated At"), auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.email
